8052302bacc07f8c3d1793d338e0f7dde97eab11
Heini <ahokas.heini@gmail.com>
2023-10-03 16:05:05 +0300

    Organize code

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,46 +1,18 @@
-import { app, BrowserWindow, dialog, ipcMain } from 'electron';
-import { readFile, writeFile } from 'fs/promises';
-import { join } from 'path';
-
-const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    show: false,
-    webPreferences: {
-      preload: join(__dirname, 'preload.js'),
-    },
-  });
-
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(
-      join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
-    );
-  }
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-    mainWindow.focus();
-  });
-
-  mainWindow.webContents.openDevTools({
-    mode: 'detach',
-  });
-};
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { createWindow } from './mainWindow';
+import { showOpenDialog, showSaveDialog } from './fileOperations';
 
 app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
   }
 });
 
@@ -59,62 +31,3 @@ ipcMain.on('show-save-dialog', (event, content: string) => {
 
   showSaveDialog(browserWindow, content);
 });
-
-const showOpenDialog = async (browserWindow: BrowserWindow) => {
-  try {
-    const result = await dialog.showOpenDialog(browserWindow, {
-      properties: ['openFile'],
-      filters: [{ name: 'Markdown File', extensions: ['md'] }],
-    });
-
-    if (result.canceled) return;
-
-    const [filePath] = result.filePaths;
-
-    openFile(browserWindow, filePath);
-  } catch (err) {
-    console.error('Falied to open dialog', err);
-  }
-};
-
-const showSaveDialog = async (
-  browserWindow: BrowserWindow,
-  content: string
-) => {
-  try {
-    const result = await dialog.showSaveDialog(browserWindow, {
-      properties: ['createDirectory'],
-      filters: [{ name: 'HTML File', extensions: ['html'] }],
-    });
-
-    if (result.canceled || !result.filePath) return;
-
-    saveFile(browserWindow, result.filePath, content);
-  } catch (err) {
-    console.error('Falied to save dialog', err);
-  }
-};
-
-const openFile = async (browserWindow: BrowserWindow, filePath: string) => {
-  try {
-    const content = await readFile(filePath, { encoding: 'utf-8' });
-
-    browserWindow.webContents.send('file-opened', content);
-  } catch (err) {
-    console.error('Failed to open file', err);
-  }
-};
-
-const saveFile = async (
-  browserWindow: BrowserWindow,
-  filePath: string,
-  content: string
-) => {
-  try {
-    await writeFile(filePath, content, { encoding: 'utf-8' });
-
-    browserWindow.webContents.send('file-saved', filePath);
-  } catch (err) {
-    console.error('Failed to save file', err);
-  }
-};
